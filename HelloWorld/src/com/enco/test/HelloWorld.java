754fce1ae1673bc636e1e1dee1f67d1daa055dc0
Enco <2480613050@qq.com>
2026-06-08 23:24:57 +0800

    HelloWorld

--- HelloWorld/src/com/enco/test/HelloWorld.java
+++ HelloWorld/src/com/enco/test/HelloWorld.java
@@ -1,7 +1,28 @@
 package com.enco.test;
 
+import java.util.Scanner;
+
+//demo test HelloWorld
 public class HelloWorld {
     public static void main(String[] args) {
+        /*
+        print Hello World
+        */
+
+        //print Hello World
         System.out.println("Hello World!");
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        int x = add(a,b);
+        System.out.println( x);
+        sc.close();
+    }
+
+    public static int add(int a, int b) {
+        int c;
+        c = a + b;
+        return c;
     }
 }

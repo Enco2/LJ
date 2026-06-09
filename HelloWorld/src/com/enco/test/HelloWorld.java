d62a2a3e1f864707cc129b0188fbc75425a1dfc8
Enco <2480613050@qq.com>
2026-06-09 21:48:16 +0800

    Literal Quantity

--- HelloWorld/src/com/enco/test/HelloWorld.java
+++ HelloWorld/src/com/enco/test/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.enco.test;
 
+import javax.print.DocFlavor;
 import java.util.Scanner;
 
 //demo test HelloWorld
@@ -10,7 +11,7 @@ public class HelloWorld {
         */
 
         //print Hello World
-        System.out.println("Hello World!");
+        /*System.out.println("Hello World!");
         int a,b;
         Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
@@ -18,6 +19,26 @@ public class HelloWorld {
         int x = add(a,b);
         System.out.println( x);
         sc.close();
+
+        char ch = 'a';
+        System.out.println(ch);
+        String str = "hello world";
+        System.out.println(str);
+        int i = 10;
+        System.out.println(i);
+        float f = 10.1f;
+        System.out.println(f);
+        double d = 10.1;
+        System.out.println(d);
+        boolean bool = true;
+        System.out.println(bool);
+        */
+
+        String name = "霸王龙";
+        int age = 8;
+        double height = 11.5;
+        char sex = '公';
+        System.out.println("姓名：" + name + "\n" + "年龄：" + age + "\n" + "身高：" + height + "\n" + "性别：" + sex);
     }
 
     public static int add(int a, int b) {
---
+++ LiteralQuantity/src/com/enco/literal/LiteralQuantity.java
@@ -0,0 +1,13 @@
+package com.enco.literal;
+
+public class LiteralQuantity {
+    public static void main(String[] args) {
+        String name = "霸王龙";
+        int age = 18;
+        double height = 11.5;
+        char sex = '公';
+
+        System.out.println("姓名：" + name + "\n" + "年龄：" + age + "\n" + "身高：" + height + "\n" + "性别：" + sex);
+
+    }
+}
